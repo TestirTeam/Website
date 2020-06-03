f5662328921ad1adbfff0d42bbe4a52b13a868b6
zexalo <zexal99@icloud.com>
2020-06-03 21:36:12 +0200

    test db connect

--- MVC/models/connexiondb.php
+++ MVC/models/connexiondb.php
@@ -1,6 +1,14 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = "a861f851@eu-cdbr-west-03.cleardb.net";
+$username = "b62629c2849eaf";
+$password = "a861f851";
+$dbb = substr($url["path"], 1);
+$dbname="heroku_b43668dd81da441";
+
 try {
-    $db = new PDO("mysql:host=localhost;dbname=bdd_testir;port=3309", "root", "root");
+    $db = new PDO("mysql:host=".$server."; dbname=".$dbname, $username, $password);
     $db->exec('SET NAMES utf8');
 
 } catch (PDOException $e) {
