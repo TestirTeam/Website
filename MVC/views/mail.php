966fd04e20cb043de42ee62b982a25e62506c358
zexalo <zexal99@icloud.com>
2020-06-03 23:16:46 +0200

    mail test

--- MVC/views/mail.php
+++ MVC/views/mail.php
@@ -1,11 +1,54 @@
 <?php
-require ("models/connexiondb.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+/*require ("models/connexiondb.php");*/
 if(isset($_SESSION['id']) and !empty($_SESSION['id'])) {
-    require("controllers/Envoimsg.php")
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'salim.rezaiguia15@gmail.com';                     // SMTP username
+        $mail->Password   = 'Salimdu95870';                               // SMTP password
+        $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('testir-team@gmail.com', 'Administrateur Testir');
+        $mail->addAddress('marie.lemercier@icloud.com');     // Add a recipient
+        //$mail->addAddress('ellen@example.com');               // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        // Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Testir Test Mail';
+        $mail->Body    = 'CA MARCHE INCHALLAH';
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    /*require("controllers/Envoimsg.php")*/
     ?>
-    <?php require('header.php')?>
+    <?php /*require('header.php')*/?>
 
-
+<!--
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="stylesheetMVC/envoi.css">
@@ -17,7 +60,7 @@ if(isset($_SESSION['id']) and !empty($_SESSION['id'])) {
         <div class="rectangle1"> </div>
         <div class="demande">
             <h1> Envoi Mail - Administrateur Testir </h1>
-            <b><p class="prems">  Bienvenue <?= $_SESSION["pseudo"] ?> !</p></b>
+            <b><p class="prems">  Bienvenue  !</p></b>
             <p class="prems"> Veuillez utiliser la saisie ci-dessous pour envoyer un mail d'aide à Testir !</p>
             <div class="rectangle2"> </div>
 
@@ -42,9 +85,7 @@ if(isset($_SESSION['id']) and !empty($_SESSION['id'])) {
                     </a>
                 </div>
                 <br/><br/>
-                <label class="error"><?php if (isset($error)) {
-                        echo $error;
-                    } ?></label>
+                <label class="error"></label>
 
             </div>
         </form>
@@ -52,9 +93,9 @@ if(isset($_SESSION['id']) and !empty($_SESSION['id'])) {
 
 
 
-    </body>
-    <?php require('footer.php')?>
+    </body> -->
+    <?php /*require('footer.php')*/?>
     <?php
 }
 ?>
-<script src="javascript/select.js"></script>
+
